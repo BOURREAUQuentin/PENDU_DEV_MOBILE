39043c17defdd38e6fed587eee4f7c1522d69b67
Bourreau Quentin <o22202632@info21-27.iut45.univ-orleans.fr>
2024-03-20 10:49:36 +0100

    feat: page rules avec icones

---
+++ pendu_dev_mobile/lib/UI/rules.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Rules extends StatelessWidget {
+  const Rules({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pendu Dev Mobile')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center( // Ajout du titre centré
+              child: Text(
+                'Rules',
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold), // Taille et style de police du titre
+              ),
+            ),
+            SizedBox(height: 80), // Espacement entre le titre et les autres éléments
+            Text('Vous avez un nombre limité de tentatives pour deviner le mot secret avant que le dessin du pendu ne soit complété.'),
+            SizedBox(height: 20), // Espacement entre les textes
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Il y a 3 niveaux possibles dans le jeu :\nniveau 1 : 2 lettres au départ sont visibles\nniveau 2 : 1 lettre au départ sont visibles\nniveau 3 : Aucune lettre au départ n’est visible'),
+                SizedBox(width: 20), // Espacement entre le texte et l'image
+                Image.asset(
+                  'icones/niveaux.png', // Chemin de l'image
+                  width: 50, // Largeur de l'image
+                  height: 50, // Hauteur de l'image
+                ),
+              ],
+            ),
+            SizedBox(height: 20), // Espacement entre les textes
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Si le pendu est complété avant que vous ne deviniez le mot, vous perdez la partie.'),
+                SizedBox(width: 20), // Espacement entre le texte et l'image
+                Image.asset(
+                  'icones/gameover.png', // Chemin de l'image
+                  width: 50, // Largeur de l'image
+                  height: 50, // Hauteur de l'image
+                ),
+              ],
+            ),
+            SizedBox(height: 20), // Espacement entre les textes
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Si vous devinez le mot avant que le pendu ne soit complété, vous remportez la partie.'),
+                SizedBox(width: 20), // Espacement entre le texte et l'image
+                Image.asset(
+                  'icones/win.png', // Chemin de l'image
+                  width: 50, // Largeur de l'image
+                  height: 50, // Hauteur de l'image
+                ),
+              ],
+            ),
+            SizedBox(height: 20), // Espacement entre les textes
+            Text('Proposez une lettre à la fois en suggérant une lettre qui pourrait être présente dans le mot secret :\nSi la lettre est correcte, elle sera révélée dans les emplacements appropriés du mot secret.\nSi la lettre est incorrecte, une partie du pendu sera dessinée pour indiquer votre erreur et la barre d\'erreurs augmentera en conséquence.'),
+            SizedBox(height: 20), // Espacement entre les textes
+            Text('Le jeu se termine lorsque vous devinez correctement le mot secret ou lorsque le dessin du pendu est complet.'),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- pendu_dev_mobile/lib/main.dart
+++ pendu_dev_mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'UI/home.dart';
+import 'UI/rules.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: Rules(),
     );
   }
 }
